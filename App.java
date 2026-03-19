55a93fc5b68232f4032e0c7e773bb65a3a9ccf9e
Narsimha <55657244+reddynarsimha2015@users.noreply.github.com>
2026-03-19 17:35:34 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,8 @@
+class App{
+public static void main(String[] args){
+System.out.println("App application");
+System.out.println("add code to jenkin repository");
+System.out.println("-------------------");
+
+}
+}
