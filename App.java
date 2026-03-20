e24c2ae79de1c3913b2bdf5cac4ed739f70f5425
Narsimha <55657244+reddynarsimha2015@users.noreply.github.com>
2026-03-20 10:19:21 +0530

    Update App.java

--- App.java
+++ App.java
@@ -3,6 +3,6 @@ public static void main(String[] args){
 System.out.println("App application");
 System.out.println("add code to jenkin repository");
 System.out.println("-------------------");
-
+System.out.println(" END APP  repository");
 }
 }
